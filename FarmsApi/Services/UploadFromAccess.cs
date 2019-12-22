ed743362d7c874206e56c47064426fcfd936bb94
tzahi <tzahi556@gmail.com>
2019-12-22 15:42:11 +0200

    hh

--- FarmsApi/Services/UploadFromAccess.cs
+++ FarmsApi/Services/UploadFromAccess.cs
@@ -15,32 +15,58 @@ namespace FarmsApi.Services
             String connection = @"Provider=Microsoft.Jet.OLEDB.4.0;" +
                                 @"Data source=C:\DataBase\Amir.mdb;Jet OLEDB:Database Password=diana;";
 
+            DataSet ds = new DataSet();
 
-            string sql = @" SELECT top 50  fm.RiderId,r.FamilyName,r.PrivateName,
-                             fm.DayofRide,fm.HourofRide,fm.executed,
-                             fm.UnexecutedReson,fm.Price,fm.invoice,w.FirstName,w.FamilyName
-                             FROM FarmDairy fm 
-                            inner join Riders r on r.RiderId=fm.RiderId
-                            inner join Workers w on w.WorkerId=fm.WorkerId
-
+            ds.Tables.Add(new DataTable("Riders"));
+            ds.Tables.Add(new DataTable("Instructor"));
+            ds.Tables.Add(new DataTable("Lessons"));
+           
 
 
-
+            string sqlLessons = @" SELECT top 50  fm.RiderId,r.FamilyName,r.PrivateName,r.PayArrangement,
+                            fm.DayofRide,fm.HourofRide,fm.executed,
+                            fm.UnexecutedReson,fm.Price,fm.invoice,
+                            w.FirstName,w.FamilyName
+                            FROM (FarmDairy fm 
+                            inner join Riders r on r.RiderId=fm.RiderId)
+                            inner join Workers w on w.WorkerID=fm.WorkerId
                         ";
+            string sqlRiders = @" SELECT * from Riders ";
+
+
+            string sqlInstructor = @" SELECT * from Workers ";
+
+
+
             using (OleDbConnection conn = new OleDbConnection(connection))
             {
                 conn.Open();
-                DataSet ds = new DataSet();
-               // DataGridView dataGridView1 = new DataGridView();
-                using (OleDbDataAdapter adapter = new OleDbDataAdapter(sql, conn))
-                {
-                    adapter.Fill(ds);
-                 //   dataGridView1.DataSource = ds;
-                    // Of course, before addint the datagrid to the hosting form you need to 
-                    // set position, location and other useful properties. 
-                    // Why don't you create the DataGrid with the designer and use that instance instead?
-                   // this.Controls.Add(dataGridView1);
-                }
+
+
+                OleDbDataAdapter da = new OleDbDataAdapter(sqlRiders, connection);
+                da.Fill(ds, "Riders");
+
+                da.SelectCommand.CommandText = sqlInstructor;
+                da.Fill(ds, "Instructor");
+
+                da.SelectCommand.CommandText = sqlLessons;
+                da.Fill(ds, "Lessons");
+                //OleDbDataAdapter adapter = new OleDbDataAdapter(sql, conn);
+
+                //adapter.Fill(ds, "Riders");
+
+
+                // DataSet ds = new DataSet();
+                // DataGridView dataGridView1 = new DataGridView();
+                //using (OleDbDataAdapter adapter = new OleDbDataAdapter(sql, conn))
+                //{
+                //    adapter.Fill(ds);
+                // //   dataGridView1.DataSource = ds;
+                //    // Of course, before addint the datagrid to the hosting form you need to 
+                //    // set position, location and other useful properties. 
+                //    // Why don't you create the DataGrid with the designer and use that instance instead?
+                //   // this.Controls.Add(dataGridView1);
+                //}
             }
 
         }
